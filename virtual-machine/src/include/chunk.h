1043b70a936ab18e32beb52a244dbdecad513ee8
Ola-jed <olabijed@gmail.com>
2024-06-15 17:13:23 +0100

    wip

--- virtual-machine/src/include/chunk.h
+++ virtual-machine/src/include/chunk.h
@@ -1,8 +1,20 @@
 #ifndef CHUNK_H
 #define CHUNK_H
+#include <cstdint>
 
 enum class OpCode
 {
     OP_RETURN
+};
+
+struct Chunk
+{
+    int count;
+    int capacity;
+    uint8_t *code;
+
+    Chunk(): count(0), capacity(0), code(nullptr)
+    {
+    }
 };
 #endif //CHUNK_H
