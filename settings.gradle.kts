8ca25852e71735fbf1977a65f1a45debfe77aa88
Ola-jed <olabijed@gmail.com>
2024-10-27 10:37:53 +0100

    Cleaned removed module ast-gen from settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 rootProject.name = "yaupl"
-include("ast-gen", "runner", "jvm", "lang")
+include("runner", "jvm", "lang")
